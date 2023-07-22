c97290bc26985edae49c20f6ed84618dc318be0b
Ivan Harbatsenka <1995van95@gmail.com>
2023-07-22 14:30:39 +0300

    Исправил клавиатуру, теперь работает корректно

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,6 @@
 from aiogram import types, Dispatcher
 from create_bot import bot
-from keyboards import client_keyboards
+from keyboards.client_keyboards import key_board
 
 
 async def start_command(message: types.Message):
@@ -9,7 +9,7 @@ async def start_command(message: types.Message):
                                                  f'\n1) Поиск отелей.'
                                                  f'\n2) Поиск ресторанов, кафе.'
                                                  f'\n3) Могу узнать погоду в интересующем вас городе.',
-                           reply_markup=client_keyboards)
+                           reply_markup=key_board)
 
 
 def register_handler_start(dp: Dispatcher):
