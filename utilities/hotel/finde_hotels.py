c33f9d82bd0ffb2d3d996400454add4075ae2123
Ivan Harbatsenka <1995van95@gmail.com>
2023-07-26 19:46:18 +0300

    Немного исправил фильтры

--- utilities/hotel/finde_hotels.py
+++ utilities/hotel/finde_hotels.py
@@ -2,11 +2,12 @@ from create_bot import hotel_key
 import requests
 
 
-async def create_hotels_dict(city_id, checkin_date, checkout_date):
+async def create_hotels_dict(city_id, checkin_date, checkout_date, page_number=0, adults_number=1):
     querystring = {"checkin_date": checkin_date, "dest_type": "city", "units": "metric", "checkout_date": checkout_date,
-                   "adults_number": "2", "order_by": "popularity", "dest_id": city_id, "filter_by_currency": "USD",
+                   "adults_number": adults_number, "order_by": "popularity", "dest_id": city_id,
+                   "filter_by_currency": "USD",
                    "locale": "ru", "room_number": "1", "children_number": "2", "children_ages": "5,0",
-                   "categories_filter_ids": "class::2,class::4,free_cancellation::1", "page_number": "0",
+                   "categories_filter_ids": "class::2,class::4,free_cancellation::1", "page_number": page_number,
                    "include_adjacency": "true"
                    }
 
