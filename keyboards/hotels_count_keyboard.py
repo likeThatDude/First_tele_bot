9d600ebfeb08c2030f93830072a0864842e69e6e
Ivan Harbatsenka <1995van95@gmail.com>
2023-07-29 16:44:24 +0300

    Исправления в создании кнопок для кол-ва отелей, из-за ограничения телеграма и rapidapi

--- keyboards/hotels_count_keyboard.py
+++ keyboards/hotels_count_keyboard.py
@@ -3,9 +3,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 async def get_count():
     keyboard = InlineKeyboardMarkup(row_width=1)
-    keyboard.add(InlineKeyboardButton('5', callback_data='//5'))
-    keyboard.add(InlineKeyboardButton('8', callback_data='//10'))
-    keyboard.add(InlineKeyboardButton('10', callback_data='//15'))
-    keyboard.add(InlineKeyboardButton('20', callback_data='//20'))
-    keyboard.add(InlineKeyboardButton('30', callback_data='//30'))
+    keyboard.add(InlineKeyboardButton('3', callback_data='//3'))
+    keyboard.add(InlineKeyboardButton('7', callback_data='//7'))
+    keyboard.add(InlineKeyboardButton('10', callback_data='//10'))
     return keyboard
